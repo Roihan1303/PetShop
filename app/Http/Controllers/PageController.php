15df471768ececa437857944ba1f7b7bd89f9d66
roihanfs <41983891+Roihan1303@users.noreply.github.com>
2024-07-01 01:19:17 +0700

    Update Controller for Order Logic

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Hewan;
+use App\Models\Order;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,7 +14,8 @@ class PageController extends Controller
     public function dashboard()
     {
         $dataHewan = Hewan::all();
-        return view('dashboard', compact('dataHewan'));
+        $order = Order::all();
+        return view('dashboard', compact('dataHewan', 'order'));
     }
 
     public function profile()
@@ -36,5 +38,17 @@ class PageController extends Controller
         }
 
         return redirect(route('profile'))->with('message', 'Profile Berhasil Diubah');
+    }
+
+    public function myOrder()
+    {
+        $dataOrder = Order::with('hewan')->where('users_id', auth()->user()->id)->get();
+        return view('order.myOrder', compact('dataOrder'));
+    }
+
+    public function orderList()
+    {
+        $dataOrder = Order::with(['hewan', 'users'])->get();
+        return view('order.orderList', compact('dataOrder'));
     }
 }
